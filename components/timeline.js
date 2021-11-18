6e6d50cc2c7914eb3e6c55c85bf633cb2c7e957d
Martin Erpicum <martin@erpicum.net>
2021-11-18 12:08:52 +0100

    testing a dummy example with d3

--- components/timeline.js
+++ components/timeline.js
@@ -1,3 +1,42 @@
 function timelineAnchorClick() {
     $('.timelineBody').toggle();
 }
+
+// Dummy example with d3.js
+data = [    {
+    "type": "Irruption",
+    "value": "5"
+}, {
+    "type": "Tsunamis",
+    "value": "2"
+}, {
+    "type": "Earthquakes",
+    "value": "4"
+}];
+
+
+d3.select("#timeline").selectAll("div")
+    .data(data)
+    .enter()
+    .append("div")
+    .attr("class", "event")
+    .style("width", function(d) {
+        return d.value * 10 + "px";
+    }
+    )
+    .style("height", function(d) {
+        return 15 + "px";
+    }
+    )
+    .style("background-color", function(d) {
+        return "rgb(100,0," + d.value * 10 + ")";
+    }
+    )
+    .style("color", function(d) {
+        return "rgb(0,0," + d.value * 10 + ")";
+    }
+    )
+    .text(function(d) {
+        return d.type;
+    }
+    );
