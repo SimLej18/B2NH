b7364f68e482c83d857e377ef37be81acdf43f42
Martin Erpicum <martin@erpicum.net>
2021-11-18 12:47:45 +0100

    🌏 map with api call

--- components/map.js
+++ components/map.js
@@ -16,7 +16,63 @@ getData().then(data => {
 // function to create map with d3.js
 const createMap = (data) => {
     console.log(data);
-    // create map
+    // create map with d3.js
+    const width = 960;
+    const height = 600;
+
+    const map = d3.select('#map')
+        .append('svg')
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .attr('viewBox', '0 0 ' + width + ' ' + height)
+        .attr('preserveAspectRatio', 'xMidYMid meet')
+        .attr('class', 'map');
+
+    // create projection
+    const projection = d3.geoMercator()
+        .center([0, 0])
+        .scale(width / 2 / Math.PI)
+        .translate([width / 2, height / 2]);
+
+    // create path
+    const path = d3.geoPath()
+        .projection(projection);
+        
+    // create tooltip
+    const tooltip = d3.select('body')
+        .append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0);
+
+    // insert map data from geojson data
+    map.selectAll('path')
+        .data(data.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('class', 'map-path')
+        .attr('fill', '#ccc')
+        .attr('stroke', '#000')
+        .attr('stroke-width', 0.5)
+        .on('mouseover', function(d) {
+            tooltip.transition()
+                .duration(200)
+                .style('opacity', .9);
+            tooltip.html(d.properties.name)
+                .style('left', (d3.event.pageX + 5) + 'px')
+                .style('top', (d3.event.pageY - 28) + 'px');
+        }
+        )
+        .on('mouseout', function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style('opacity', 0);
+        }
+        );
+
+
+
+
 } 
 
 
