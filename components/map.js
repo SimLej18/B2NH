1ce2961e721fd36c199fe25e14b68ec3f12edefb
Martin Erpicum <martin@erpicum.net>
2021-11-22 14:04:44 +0100

    move map div inside panelContainer and add points to maps with color binding

--- components/map.js
+++ components/map.js
@@ -1,17 +1,41 @@
+// const getData = async (url) => (
+//     await fetch(url)
+//     .then(
+//         data => data.json()
+//     )
+//   );
+  
+  const wrapperFunc = async () => {  
+    let volcanoes_map_url = "https://b2nh-api.tintamarre.be/api/v1/volcanoes_map";
+    let world_map_url = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson";
+    let world_data = await getData(world_map_url);
+    let volcanoes = await getData(volcanoes_map_url);
+    // assign data to global variable
+    // world_data = world_data;
+    // volcanoes = volcanoes;
+    // return data
+    return {
+        world_data,
+        volcanoes
+    };
+  }
+
+  
 // define empty data object to store data
 let data = {};
 let volcanoes_map_url = "https://b2nh-api.tintamarre.be/api/v1/volcanoes_map";
 let world_map_url = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson";
+let events_url = "https://b2nh-api.tintamarre.be/api/v1/filter_map/start/-5000/end/2021";
 
 // function to get data from url for d3 map
-const getData = () => {
-    return fetch(world_map_url)
+const getData = (url) => {
+    return fetch(url)
         .then(res => res.json())
         .then(data => data)
     }
 
 // when data is loaded, create map
-getData().then(data => {
+getData(events_url).then(data => {
     createMap(data);
 });
 
@@ -48,13 +72,31 @@ const createMap = (data) => {
     // create path
     const path = d3.geoPath()
         .projection(projection);
-        
+   
+    // Add a scale for bubble size
+    const size = d3.scaleLinear()
+    .domain([0,10])  // What's in the data
+    .range([ 2, 10]);  // Size in pixel
+
+
+       // Create a color scale
+       var color = d3.scaleOrdinal()
+       .domain(["VolcanoEvent", "EarthquakeEvent", "TsunamiEvent" ])
+       .range([ "red", "green", "blue"])
+
+
     // create tooltip
     const tooltip = d3.select('#info-item-tooltip')
         .append('div')
         .attr('class', 'tooltip')
         .style('size', '10px')
-        .style('background-color', '#fff');
+        .style('background-color', '#fff')
+        .style('border', 'solid')
+        .style('border-width', '1px')
+        .style('border-radius', '5px')
+        .style('padding', '10px')
+        .style('position', 'absolute')
+        .style('z-index', '10');        
 
     // insert map data from geojson data
     map.selectAll('path')
@@ -63,15 +105,18 @@ const createMap = (data) => {
         .append('path')
         .attr('d', path)
         .attr('class', 'map-path')
-        .attr('fill', 'white')
+        // .attr("r", 15)
+        .attr("r", function(d){ return size(d.properties.measure_value) })
+        .attr("fill", function(d){ return color(d.properties.type) })
         .attr('stroke', '#ccc')
         .attr('stroke-width', 1)
+        .text(function(d) { return d.properties.title })
         .on('mouseover', function(d) {
             console.log(d);
             tooltip.transition()
                 .duration(200)
                 .style('opacity', .9);
-            tooltip.html(d.properties.name)
+            tooltip.html(d.properties.title)
                 .style('left', (d3.event.pageX + 5) + 'px')
                 .style('top', (d3.event.pageY - 28) + 'px');
         }
