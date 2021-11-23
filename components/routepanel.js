d3a14055b99564610b34d2dd10ef5138b2ff0d4b
Ushien <antoinevanaelst@hotmail.com>
2021-11-23 22:58:13 +0100

    Routepanel now displays the current circuit
    The event is randomly selected

--- components/routepanel.js
+++ components/routepanel.js
@@ -5,9 +5,22 @@ function routepanelAnchorClick() {
     $('.routepanelBody').toggle();
 }
 
+// Ajoute l'evènement sélectionné au trajet
 function addDestinationClick() {
+    // Currently add random event from the allEventsList
     $('.buttonadddestination').toggle();
     console.log('A new destination is added')
-    currentroute.push(selected_event)
-    console.log('Current array: ' + currentroute)
+    currentroute.push(allEventsList[Math.floor(Math.random() * (allEventsList.length-1))]) // Change this
+    console.log('Current circuit: ' + currentroute)
+    updateRoute()
+}
+
+// Mise à jour de la fenêtre d'affichage de la route
+function updateRoute() {
+    var completelist= document.getElementById('printedRoute')
+
+    completelist.innerHTML = ""
+    for (let i = 0; i < currentroute.length; i++) {
+        completelist.innerHTML += "<li> " + currentroute[i]["title"] + "</li>";
+      } 
 }
