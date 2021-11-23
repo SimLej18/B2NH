2e77348b0469d3d546e2a58a1574636d44fa5d61
SimLej <simonlejoly@icloud.com>
2021-11-23 21:32:04 +0100

    Timeline cursors can now move

--- components/timeline.js
+++ components/timeline.js
@@ -40,15 +40,43 @@ async function createTimeline() {
 	paper.setup(canvas);
 
     with (paper) {
-        // Create the line bar
+        var tool = new Tool();
+
+        // Create the line bar and cursors
         var lineBar = new Path.Rectangle(new Point(50, 65), new Size(600, 5));
         lineBar.fillColor = "black";
 
         var leftCursor = new Path.Rectangle(new Point(50, 55), new Size(20, 25));
+        leftCursor.data.clicked = false;
         leftCursor.fillColor = "black";
 
         var rightCursor = new Path.Rectangle(new Point(630, 55), new Size(20, 25));
+        rightCursor.data.clicked = false;
         rightCursor.fillColor = "black";
+
+        // Create the cursor drag functions
+        tool.onMouseDown = function(e) {
+            if (leftCursor.contains(e.point)) {
+                leftCursor.data.clicked = true;
+            }
+            else if (rightCursor.contains(e.point)) {
+                rightCursor.data.clicked = true;
+            }
+        }
+
+        tool.onMouseUp = function(e) {
+            leftCursor.data.clicked = false;
+            rightCursor.data.clicked = false;
+        }
+
+        tool.onMouseDrag = function(e) {
+            if (leftCursor.data.clicked) {
+                leftCursor.position = new Point(Math.max(Math.min((e.point.x-5), rightCursor.position.x-20), 60), 67.5);
+            }
+            if (rightCursor.data.clicked) {
+                rightCursor.position = new Point(Math.max(Math.min((e.point.x-5), 640), leftCursor.position.x+20), 67.5);
+            }
+        }
 
     	// Draw the view now:
     	view.draw();
