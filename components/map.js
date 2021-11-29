146ac0bf4524d8628df53ea3e69a6eeb94901b86
Martin Erpicum <martin@erpicum.net>
2021-11-29 14:55:30 +0100

    zoom not yet working as expected ...

--- components/map.js
+++ components/map.js
@@ -4,13 +4,10 @@
 
 let map_data = [];
 
-//function used when selecting an event
-function replaceClass(id, oldClass, newClass) {
-  var elem = $(`#${id}`);
-  if (elem.hasClass(oldClass)) {
-      elem.removeClass(oldClass);
-  }
-  elem.addClass(newClass);
+// Toggle class : why is it here ?
+function toggleClass(id, Class) {
+  d3.select(id)
+  .classed(Class, !d3.select(id).classed(Class))
 }
 
 // Method that will be used when we update the data
@@ -23,6 +20,7 @@ function updateMap() {
 }
 
 function addDestinationToMap() {
+    toggleClass
     console.log("addDestinationToMap called!");
 }
 
@@ -36,15 +34,10 @@ function createMap() {
 function removeMap() {
   d3.select('#map')
   .selectAll('svg')
-  .remove()
-  .selectAll('path')
-  .remove()
-  .selectAll('circle')
   .remove();
 }
 
 function draw() {
-  console.log('draw called!')
 
   const width = 960;
   const height = 600;
@@ -55,11 +48,11 @@ function draw() {
 
   const map = d3.select('#map')
       .append('svg')
-      .attr('width', '100%')
-      .attr('height', '100%')
+      .attr('width', width)
+      .attr('height', height)
       .attr('viewBox', '0 0 ' + width + ' ' + height)
       .attr('preserveAspectRatio', 'xMidYMid meet')
-      .attr('class', 'map');  
+      .attr('class', 'map');
 
   // create projection
   //   Alternative
@@ -67,6 +60,7 @@ function draw() {
   //   d3.geoConicConformal()
   //   d3.geoEquirectangular()
   //   d3.geoOrthographic() // spherical
+  //  d3.geoNaturalEarth1() // globe
 
   const projection = d3.geoMercator()
       .center([4.858293, 50.46651]) // center of the map == UNamur
@@ -114,6 +108,9 @@ function draw() {
     
     // source = https://svgsilh.com/image/306989.html
     var wavePath = "M5445 11153 c-120 -8 -321 -34 -444 -58 -744 -145 -1243 -515 -1471-1089 -59 -150 -74 -234 -74 -411 -1 -185 11 -243 74 -372 57 -116 155 -199260 -219 l35 -7 -23 19 c-28 23 -54 121 -45 168 22 121 151 210 399 277 11330 125 31 244 26 455 -22 856 -291 1165 -782 70 -111 188 -352 248 -505 107-274 183 -583 234 -950 25 -176 25 -700 0 -920 -110 -983 -466 -1887 -1089-2762 -640 -901 -1581 -1762 -2673 -2445 -725 -453 -1349 -762 -2225 -1102l-55 -21 1465 5 c4858 18 6793 25 8670 30 1158 3 2205 8 2327 11 l222 6 15 68c23 101 53 296 73 470 21 193 24 791 5 1005 -80 878 -243 1639 -551 2565 -8912677 -2589 5186 -4256 6288 -666 441 -1252 646 -2005 702 -125 9 -407 11 -5253z"
+
+    var squarePath = "M0,0 L0,100 L100,100 L100,0 L0,0 z";
+    var roundPath = "M0,0 A50,50 0 0,1 100,0 A50,50 0 0,1 0,0 z";
    
      var colorScaleEarthquake = d3.scaleLinear()
      .domain([0,10])
@@ -134,7 +131,7 @@ var symbol_path = function(d) {
   else if(d.type == 'eruption') {
     return dropPath;
   }
-  else return dropPath;
+  else return roundPath;
 }
 
 var symbol_size = function(d) {
@@ -174,8 +171,10 @@ var symbol_type = d3.symbol().type(function(d) {
   .data(map_data, ({type,id}) => type + '_' + id)
   .enter()
   .append('path')
-  .attr("d", symbol_type)
-//  .append("circle")
+  .attr('d', symbol_type)
+  // .attr("d", symbol_path)
+  // .attr("size", symbol_size)
+  //  .append("circle")
   .attr("transform", function(d) {
       return "translate(" + projection([
         d.longitude,
@@ -202,8 +201,7 @@ var symbol_type = d3.symbol().type(function(d) {
         .duration(1000)
         .style('opacity', 0);
         d3.select(this)
-        .attr('fill', function(d){ return color(d) })
-        .attr("d", symbol_type);
+        .attr('fill', function(d){ return color(d) });
 })
 .on('click', function(e, d) {
     clickEvent(e, d);
@@ -214,24 +212,12 @@ var symbol_type = d3.symbol().type(function(d) {
 
 
   function zoomed(e) {
-     
-    // Ajust zooming and panning speed
+    map.attr("transform", e.transform);
+    //  map.attr('transform', "scale(" + e.transform.k + ")", "translate(" + width / 2 + "," + height / 2 + "), translate(" + e.transform.x + "," + e.transform.y + ")");
 
-    // var scale = e.transform.k;
-    // var scale_factor = scale / zoom_scale;
-    // zoom_scale = scale;
-    // var translate = e.transform.x;
-    // var translate_factor = translate / zoom_translate;
-    // zoom_translate = translate;
+    //  world_map.attr("scale(" + 1 / e.transform.k + ")");
 
-
-    // infopanelAnchorClick();
-
-     map.attr('transform', e.transform, "scale(" + e.transform.k + ")", "translate(" + e.transform.x + "," + e.transform.y + ")");
-
-     world_map.attr("scale(" + 1 / e.transform.k + ")");
-     points_map_data
-     .attr('transform', function(d) {
+     points_map_data.attr('transform', function(d) {
       // console.log('zoomin points', e.transform.k);
       return "translate(" + projection([
           d.longitude,
@@ -243,31 +229,30 @@ var symbol_type = d3.symbol().type(function(d) {
  function clickEvent(e,d) {
     //console.log(e, d);
 
-    // update centre of map
-    var centre = projection([d.longitude, d.latitude]);
+    var centroid = projection([d.longitude, d.latitude]);
+    // var centroid = projection([0.0, 0.0]);
 
+    projection([d.longitude, d.latitude]);
+    var x = centroid[0];
+    var y = centroid[1];
+
+    // update centre of map
+    console.log('centroid', centroid);
   
     // update map
     map.transition()
-    .duration(750)
-    .call(zoom.transform, d3.zoomIdentity
-      .translate(centre[0] - width / 2, centre[1] - height / 2)
-      .scale(20))
-      .on("end", function() {
-        // open info panel
-        infopanelAnchorClick();
-        // update info panel
-        updateInfoPanel(d.self_url);
-      });
-  
-    
+    .duration(1000)
+    .attr('transform', "scale(" + 20 + "), translate(" + 0 + "," + 0 + "), translate(" + x + "," + y + ")")
+    .on("end", function() {
+          // open info panel
+          infopanelAnchorClick();
+          // update info panel
+          updateInfoPanel(d.self_url);
+        });
 
     // selectedEvent = d;
     // updateCircuitButton();
 
-     // map.selectAll("path").transition()
-    // .duration(700)
-    // .attr("d", path);
 }
 
 }
