e0d3e1bebbfed953baa6eb4c4d93751c101be26d
Martin Erpicum <martin@erpicum.net>
2021-11-29 11:14:44 +0100

    fix points scaling

--- components/map.js
+++ components/map.js
@@ -70,7 +70,7 @@ function draw() {
 
   const projection = d3.geoMercator()
       .center([4.858293, 50.46651]) // center of the map == UNamur
-      .scale(width / 2 / Math.PI - 10)
+      .scale(width / 2 / Math.PI - 10) // scale to fit the map to the screen
       .translate([width / 2, height / 2]);
 
   // create path
@@ -87,7 +87,7 @@ function draw() {
   // Add a scale for bubble size
   var size = d3.scaleLinear()
    .domain([1,10])  // What's in the data
-   .range([50, 100]);  // Size in pixel
+   .range([70, 150]);  // Size in pixel
 
    // Create a color scale
    var color = function(d) { 
@@ -101,11 +101,9 @@ function draw() {
     };
 
     var dropPath = 'M 243.44676,222.01677 C 243.44676,288.9638 189.17548,343.23508 122.22845,343.23508 C 55.281426,343.23508 1.0101458,288.9638 1.0101458,222.01677 C 1.0101458,155.06975 40.150976,142.95572 122.22845,0.79337431 C 203.60619,141.74374 243.44676,155.06975 243.44676,222.01677 z';
-
-    //  <svg id="" class="drop" width="100%" height="100%">
-    //  <path d="M 243.44676,222.01677 C 243.44676,288.9638 189.17548,343.23508 122.22845,343.23508 C 55.281426,343.23508 1.0101458,288.9638 1.0101458,222.01677 C 1.0101458,155.06975 40.150976,142.95572 122.22845,0.79337431 C 203.60619,141.74374 243.44676,155.06975 243.44676,222.01677 z" fill="#fff"></path>
-    //  </svg>
-     
+    
+    // source = https://svgsilh.com/image/306989.html
+    var wavePath = "M5445 11153 c-120 -8 -321 -34 -444 -58 -744 -145 -1243 -515 -1471-1089 -59 -150 -74 -234 -74 -411 -1 -185 11 -243 74 -372 57 -116 155 -199260 -219 l35 -7 -23 19 c-28 23 -54 121 -45 168 22 121 151 210 399 277 11330 125 31 244 26 455 -22 856 -291 1165 -782 70 -111 188 -352 248 -505 107-274 183 -583 234 -950 25 -176 25 -700 0 -920 -110 -983 -466 -1887 -1089-2762 -640 -901 -1581 -1762 -2673 -2445 -725 -453 -1349 -762 -2225 -1102l-55 -21 1465 5 c4858 18 6793 25 8670 30 1158 3 2205 8 2327 11 l222 6 15 68c23 101 53 296 73 470 21 193 24 791 5 1005 -80 878 -243 1639 -551 2565 -8912677 -2589 5186 -4256 6288 -666 441 -1252 646 -2005 702 -125 9 -407 11 -5253z"
    
      var colorScaleEarthquake = d3.scaleLinear()
      .domain([0,10])
@@ -118,6 +116,27 @@ function draw() {
      var colorScaleTsunami = d3.scaleLinear()
      .domain([0, 10])
      .range(["#fff", '#0000ff']);
+
+var symbol_path = function(d) {
+  if(d.type == 'tsunami') {
+    return wavePath;
+  }
+  else if(d.type == 'eruption') {
+    return dropPath;
+  }
+  else return dropPath;
+}
+
+var symbol_size = function(d) {
+  if(d.type == 'tsunami') {
+    return size(d.measure_value);
+  }
+  else if(d.type == 'eruption') {
+    return size(d.measure_value);
+  }
+  else return size(d.measure_value);
+}
+
 
     // use this for generating symbols
 var symbol_type = d3.symbol().type(function(d) { 
@@ -185,33 +204,16 @@ var symbol_type = d3.symbol().type(function(d) {
 
 
   function zoomed(e) {
-        // g.attr("transform","translate("+ 
-        //     e.translate.join(",")+")scale("+e.scale+")");
-        // g.selectAll("circle")
-        //     .attr("d", path.projection(projection))
-        //     .attr("r", 5/zoom.scale());
-        // g.selectAll("path")  
-        //     .attr("d", path.projection(projection)); 
+     map.attr('transform', e.transform, "scale(" + e.transform.k + ")", "translate(" + e.transform.x + "," + e.transform.y + ")");
 
-     map.attr('transform', e.transform);
-    //  world_map.attr('transform', e.transform);
-
-     points_map_data.attr('transform', function(d) {
-        return "translate(" + projection([
+     points_map_data
+     .attr('transform', function(d) {
+      console.log('zoomin points', e.transform.k);
+      return "translate(" + projection([
           d.longitude,
           d.latitude
-        ]) + ")";
-      }).attr("d", symbol_type);
-      // .attr('transform', e.transform);
-  
-      // return d3.select(this).attr('r') * 1.1;
-
-
-    //   .attr('r', function(d) {
-    //     return weight_scale(d.properties.mass) / d3.event.transform.k
-    // })
-    // .attr("stroke-width", 1 / d3.event.transform.k);
-
+        ]) + ")" + "scale(" + 1 / e.transform.k + ")";
+      });
     } 
 
  function clickEvent(e,d) {
@@ -221,17 +223,7 @@ var symbol_type = d3.symbol().type(function(d) {
     // selectedEvent = d;
     // updateCircuitButton();
 
-    // var centroid = path.centroid(d),
-    // translate = projection.translate();
-
-    // projection.translate([
-    // translate[0] - centroid[0] + width / 2,
-    // translate[1] - centroid[1] + height / 2
-    // ]);
-
-    // zoom.translate(projection.translate());
-
-    // map.selectAll("path").transition()
+     // map.selectAll("path").transition()
     // .duration(700)
     // .attr("d", path);
 }
