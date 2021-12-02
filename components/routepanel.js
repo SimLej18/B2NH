8c4ee9d9068bad54bd1de667fdea8f9717e22697
Ushien <antoinevanaelst@hotmail.com>
2021-12-03 00:00:14 +0100

    Let's test some things

--- components/routepanel.js
+++ components/routepanel.js
@@ -11,8 +11,8 @@ function addDestinationClick() {
     console.log(selectedEvent)
     // Currently add random event from the allEventsList
     console.log('A new destination is added')
-    currentroute.push(allEventsList[Math.floor(Math.random() * (allEventsList.length-1))]) // Change this
-    console.log('Current circuit: ' + currentroute)
+    currentroute.push(selectedEvent)
+    console.log('Current circuit length: ' + currentroute.length)
     updateRoute()
 }
 
@@ -30,16 +30,54 @@ function updateRoute() {
     // routepanel
     var completelist= document.getElementById('printedRoute')
 
+    $('#printedRoute').empty();
     completelist.innerHTML = ""
     for (let i = 0; i < currentroute.length; i++) {
-        completelist.innerHTML += "<li>" + currentroute[i]["title"];
+
+        var line = document.createElement("P"); 
+        document.getElementById("printedRoute").appendChild(line);
+        line.id = "destination" + i
+        line.className = "destinationline"
+        // line.onclick = selectRouteEvent(i)
+        line.setAttribute("onclick","selectRouteEvent(" + i + ")");
+
+        destinationLine = ""
+
+        //destinationLine += "<p class = 'destinationline' onclick='selectRouteEvent(" + i + ")'>" ;
+
+        // add the emoji
+        destinationLine += currentroute[i]["emoji"] + " ";
+
+        // add the country
+        if (currentroute[i]["type"] == "eruption"){
+            destinationLine += currentroute[i]["volcano"]["country"]
+        }
+        else{
+            destinationLine += currentroute[i]["country"]
+        }
+
+        // add the date
+        currentdate = currentroute[i]['dateTime'];
+        index = currentdate.indexOf(',')+2
+        currentdate = currentdate.slice(index, index+11)
+        var finaldate = currentdate.replace("-", " ")
+        finaldate = finaldate.replace("-", " ")
+
+        destinationLine += " - " + finaldate
+
+        // add the buttons
+        
         if(i >= 1){
             completelist.innerHTML += " <button onclick = 'routeUp(" + i + ")'>Up</button>"
         }
         if(i < currentroute.length-1){
             completelist.innerHTML += "<button onclick = 'routeDown(" + i + ")'>Down</button>"
         }
-        completelist.innerHTML += "</li>"
+        destinationLine += "</p>"
+
+        line.innerHTML = destinationLine
+
+        //completelist.innerHTML += destinationLine
     } 
 
     if (currentroute.length == 0){
@@ -82,4 +120,49 @@ function routeDown(i){
     currentroute[i+1] = tmp
 
     updateRoute()
+}
+
+function selectRouteEvent(index){
+    console.assert(index < currentroute.length && index >= 0, "Index exception")
+    currentEvent = currentroute[index]
+
+    // clickEvent(e, currentEvent.data)
+    console.log(currentEvent)
+    updateInfoPanel(currentEvent["self_url"]);
+    selectedEvent = currentEvent;
+    updateCircuitButton();
+}
+
+// Draw the entire circuit on the world map
+function drawCircuit(){
+    if(currentroute.length > 1){
+        for (let i = 1; i < currentroute.length; i++){
+            drawLine(currentroute[i-1], currentroute[i])
+        }
+    }
+
+}
+
+// Draw a line between two destinations
+function drawLine(event1, event2)
+{
+    const canvas = document.querySelector('#canvas');
+
+    if (!canvas.getContext) {
+        return;
+    }
+    const ctx = canvas.getContext('2d');
+
+    // set line stroke and line width
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 3;
+
+    // draw a red line
+    ctx.beginPath();
+    console.log(event1)
+    ctx.moveTo(event1["latitude"], event1["longitude"]);
+    ctx.lineTo(event2["latitude"], event2["longitude"]);
+    ctx.stroke();
+    
+    draw();
 }
