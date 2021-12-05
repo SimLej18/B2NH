be6182419beb127d9d2fd1d6499e37cefbda2cb7
Martin Erpicum <martin@erpicum.net>
2021-12-05 16:52:09 +0100

    #4 coding without testing is like swimming without swimsuit

--- components/map.js
+++ components/map.js
@@ -75,12 +75,6 @@ function updateMap() {
   updateEvents(map_data);
 }
 
-function addDestinationToMap(currentroute) {
-  // toggleClass
-  console.log(currentroute);
-  console.log('addDestinationToMap called!');
-}
-
 // function to create map with d3.js
 function createMap() {
   map_data = filteredEvents; // allEventsList;
@@ -179,11 +173,9 @@ function draw() {
   var toBase10 = function (d) {
     if (d.type == 'eruption') {
       measureBase10 = d.measure_value / 8 * 10;
-    }
-    else if (d.type == 'earthquake') {
+    } else if (d.type == 'earthquake') {
       measureBase10 = (d.measure_value - 6.5) * 4; // Scale adaptation
-    }
-    else measureBase10 = d.measure_value;
+    } else measureBase10 = d.measure_value;
 
     return measureBase10;
   }
@@ -244,11 +236,13 @@ function draw() {
     .attr('fill', function (d) {
       return color(d)
     })
-    .text(function (d) { return d.measure_value; })
+    .text(function (d) {
+      return d.measure_value;
+    })
     .attr('fill-opacity', 0.5)
     .on('mouseover', function (e, d) {
       tooltip.attr('id', 'info_event_tooltip')
-        .attr('class','sb5')
+        .attr('class', 'sb5')
         .transition()
         .duration(200)
         .style('opacity', 1);
@@ -260,9 +254,9 @@ function draw() {
         emoji = '🌋';
       } else emoji = '🌏';
 
-      tooltip.html(emoji + ' <strong>' + d.title + '</strong><br/><br/><strong>' + d.measure_type + '</strong>: <span style="color:white;">' + d.measure_value + '</span>' + '<br/><br/><em>' +  d.dateTimeForHumans + '</em>')
-      .style("left", (e.pageX-2) + "px")
-      .style("top", (e.pageY - 140) + "px");	;
+      tooltip.html(emoji + ' <strong>' + d.title + '</strong><br/><br/><strong>' + d.measure_type + '</strong>: <span style="color:white;">' + d.measure_value + '</span>' + '<br/><br/><em>' + d.dateTimeForHumans + '</em>')
+        .style("left", (e.pageX - 2) + "px")
+        .style("top", (e.pageY - 140) + "px");;
 
       showCoordinates(e, d);
 
@@ -292,7 +286,7 @@ function draw() {
         .duration(1000)
         .style('opacity', 0);
 
-        d3.select("#lat_long_info")
+      d3.select("#lat_long_info")
         .transition()
         .duration(1000)
         .style('opacity', 0)
@@ -340,14 +334,14 @@ function draw() {
 
 
 
-    function showCoordinates(e, d) {
-      d3.select('#lat_long_info').remove();
+  function showCoordinates(e, d) {
+    d3.select('#lat_long_info').remove();
 
-      d3.select('#coordinates_tooltip')
+    d3.select('#coordinates_tooltip')
       .append('div')
       .attr('id', 'lat_long_info')
       .html('<code>Latitude ' + d.latitude + ' Longitude: ' + d.longitude + '</code>');
-     }
+  }
 
 
   function zoomed(e) {
@@ -384,8 +378,9 @@ function draw() {
   function clickEvent(e, d) {
 
     // close panels
-    if ($('.infopanelBody').is(":hidden")){
-    infopanelAnchorClick();}
+    if ($('.infopanelBody').is(":hidden")) {
+      infopanelAnchorClick();
+    }
     //routepanelAnchorClick();
 
     //routepanelAnchorClick()
@@ -399,7 +394,7 @@ function draw() {
     // then performs zoom
 
     position = projection([d.longitude, d.latitude]);
-    if(currentZoom >= 13) {
+    if (currentZoom >= 13) {
       scale = 15;
     } else {
       scale = currentZoom + 3;
@@ -418,7 +413,7 @@ function draw() {
       .on('end', function () {
 
       });
-     }
+  }
 
 
 
@@ -436,24 +431,91 @@ function highlightEventOnMap(e, d) {
   var map = d3.select('#map').select('svg');
 
   map
-  .append('circle')
-  .attr('cx', cx)
-  .attr('cy', cy)
-  .attr('r', '800')
-  .attr('stroke', 'red')
-  .attr('stroke-width', '5px')
-  .attr('fill', 'none')
-  .transition()
-  .duration(500)
-  .attr('r', '20')
-  .attr('stroke-width', '4px')
-  .on('end', function () {
-    d3.select(this)
-      .transition()
-      .duration(500)
-      .attr('r', '2000')
-      .attr('stroke-width', '12px')
-      .remove();
-  });
+    .append('circle')
+    .attr('cx', cx)
+    .attr('cy', cy)
+    .attr('r', '800')
+    .attr('stroke', 'red')
+    .attr('stroke-width', '5px')
+    .attr('fill', 'none')
+    .transition()
+    .duration(500)
+    .attr('r', '20')
+    .attr('stroke-width', '4px')
+    .on('end', function () {
+      d3.select(this)
+        .transition()
+        .duration(500)
+        .attr('r', '2000')
+        .attr('stroke-width', '12px')
+        .remove();
+    });
 
-}
+
+
+
+  function addDestinationToMap(currentroute) {
+    // toggleClass
+    for (let i = 0; i < currentroute.length; i++) {
+
+      if (i != currentroute.length - 1) {
+
+        let current = currentroute[i];
+        let next = currentroute[i + 1];
+        
+        var current_point = d3.select('#' + current.type + '_' + current.id);
+        var current_point_coordinates = current_point.attr('transform');
+      
+        var x1 = current_point_coordinates.split('(')[1].split(',')[0];
+        var y1 = current_point_coordinates.split('(')[1].split(',')[1].split(')')[0];
+         
+        var next_point = d3.select('#' + next.type + '_' + next.id);
+        var next_point_coordinates = next_point.attr('transform');
+    
+        var x2 = next_point_coordinates.split('(')[1].split(',')[0];
+        var y2 = next_point_coordinates.split('(')[1].split(',')[1].split(')')[0];
+
+        var map = d3.select('#map').select('svg');
+
+        // let angle = getAngle(current_lat, current_long, next_lat, next_long);
+        
+        // let name = current.name;
+        // let description = current.description;
+        
+        // let longitude = current.longitude;
+        // let latitude = current.latitude;
+        // let id = current.id;
+        // let marker = {
+        //   'name': name,
+        //   'description': description,
+        //   'longitude': longitude,
+        //   'latitude': latitude,
+        //   'angle': angle,
+        //   'id': id
+        // };
+
+        var map = d3.select('#map').select('svg');
+
+        map
+          .append('line')
+          .attr('x1', x1)
+          .attr('y1', y1)
+          .attr('x2', x2)
+          .attr('y2', y2)
+          .attr('stroke', 'red')
+          .attr('stroke-width', '5px')
+          .attr('fill', 'none')
+          .transition()
+          .duration(500)
+          .attr('x2', x)
+          .attr('y2', y)
+          .attr('stroke-width', '4px');
+
+      }
+
+      console.log(currentroute);
+      console.log('addDestinationToMap called!');
+    }
+
+
+  }
